34b7343cd6020d8d0cd6fb7073a714245a8f7c1c
robinbutler <r.l.butler40@gmail.com>
2021-04-04 19:57:56 +0100

    created fetchWeatherData function to retrieve degreeDays from API

--- api.js
+++ api.js
@@ -20,6 +20,22 @@ const readHouseJSON = (filepath) => {
     }));
     console.log(billingForm);
   };
+
+  const fetchWeatherData = (location) => {
+    return axios
+      .get(
+        `https://063qqrtqth.execute-api.eu-west-2.amazonaws.com/v1/weather?location=${location}`,
+        { headers: { "x-api-key": "f661f74e-20a7-4e9f-acfc-041cfb846505" } }
+      )
+      .then(({ data }) => {
+        if (typeof data.location.degreeDays !== "undefined") {
+          return parseInt(data.location.degreeDays, 10);
+        }
+      })
+      .catch(function (error) {
+        return null;
+      });
+  };
 };
 
 readHouseJSON("./data/houses.json");
